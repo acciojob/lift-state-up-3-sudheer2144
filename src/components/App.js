9eae8174d6bd41f9416483287f96670e46ee3901
sudheer2144 <sudheer.ged@gmail.com>
2023-08-18 23:20:06 +0530

    submitting code-Fri Aug 18 2023 23:20:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [option, setOption] = useState("");
+
+  function optionSet(e) {
+    setOption(e.target.textContent)
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div>
+        <h2>Child Component 1</h2>
+        <button onClick={optionSet}>Option 1</button>
+      </div>
+      <div>
+        <h2>Child Component 2</h2>
+        <button onClick={optionSet}>Option 2</button>
+      </div>
+      <p>Selected Option: {option}</p>
     </div>
   )
 }
